ef6e8746cfc59756483901e5f62f458e7e27fa0a
JoachimCoenen <joachimcoenen@icloud.com>
2017-12-05 11:22:09 +0100

    created PostProcessingSettingsBase class

---
+++ Cat/PostProcessing/Common/PostProcessingSettingsBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cat.Common;
+namespace Cat.PostProcessing {
+	
+	//[Serializable]
+	public class PostProcessingSettingsBase : ScriptableObject {
+
+		public static PostProcessingSettingsBase defaultSettings { 
+			get {
+				return new PostProcessingSettingsBase();//This();
+			}
+		}
+
+		public PostProcessingSettingsBase Copy() {
+			var original = this;
+			var copy = defaultSettings;
+
+
+			var fields = original.GetType().GetFields();
+			foreach (var field in fields) {
+				field.DeclaringType.IsArray;
+				object fieldValue = field.GetValue(original, null);
+
+				if (fieldValue is Array) {
+					CopyArrayMembers(fieldValue, field);
+				}
+
+			}
+
+			copy.MemberwiseClone();
+		}
+
+		private void CopyArrayMembers(object fieldValue, FieldInfo field) {
+			Debug.Assert(fieldValue is Array);
+
+			var array = fieldValue as Array;
+			foreach (var member in array) {
+				member;
+			}
+			
+		}
+			
+	}
+}
